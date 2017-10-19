83216dac01194941b6d4ba3a08e4141b5d5053af
Rene Vallecillo <reneval@gmail.com>
2017-10-18 20:53:48 -0600

    basic flex layout

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    imports: [ProductsRoutingModule],
+    imports: [
+        ProductsRoutingModule,
+        SharedModule
+    ],
     exports: [],
     declarations: [ProductsComponent],
     providers: [],
